5f759cc9e70070483e3f51624c1d4cca436bcf84
Burd573 <48254256+Burd573@users.noreply.github.com>
2021-06-17 12:53:22 -0700

    Update Performer.java

--- Assignment4/activity1/src/main/java/taskone/Performer.java
+++ Assignment4/activity1/src/main/java/taskone/Performer.java
@@ -1,170 +1,68 @@
 /**
-  File: Performer.java
+  File: Server.java
   Author: Student in Fall 2020B
-  Description: Performer class in package taskone.
+  Description: Server class in package taskone.
 */
 
 package taskone;
 
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 import org.json.JSONObject;
 
 /**
- * Class: Performer 
- * Description: Threaded Performer for server tasks.
+ * Class: Server
+ * Description: Server tasks.
  */
-public class Performer {
+class Server {
 
-    private StringList state;
-    private Socket conn;
+    public static void main(String[] args) throws Exception {
+        int port;
+        StringList strings = new StringList();
 
-    public Performer(Socket sock, StringList strings) {
-        this.conn = sock;
-        this.state = strings;
-    }
-
-    public JSONObject quit() {
-        JSONObject json = new JSONObject();
-        json.put("datatype", 0);
-        json.put("type", "quit");
-        json.put("data", String.valueOf(this.state.size()));
-        System.out.println("JSON to send: " + json);
-        return json;
-    }
-
-    public JSONObject add(String str) {
-        JSONObject json = new JSONObject();
-        json.put("datatype", 1);
-        json.put("type", "add");
-        state.add(str);
-        json.put("data", state.toString());
-        return json;
-    }
-
-    public JSONObject pop(){
-        JSONObject json = new JSONObject();
-        json.put("datatype", 2);
-        json.put("type", "pop");
-        if(state.strings.isEmpty()) {
-            json.put("data","empty");
-        } else{
-            String top = state.strings.get(state.size()-1);
-            json.put("data",top);
-            state.strings.remove(state.size()-1);
+        if (args.length != 1) {
+            // gradle runServer -Pport=9099 -q --console=plain
+            System.out.println("Usage: gradle runServer -Pport=9099 -q --console=plain");
+            System.exit(1);
         }
-        System.out.println("Sending " + json + " to server");
-        return json;
-    }
-
-    public JSONObject display(){
-        String display = "";
-        JSONObject json = new JSONObject();
-        json.put("datatype", 3);
-        json.put("type", "display");
-        for(String s : state.strings){
-            display +=  s + "\n";
-        }
-        json.put("data",state.toString());
-        return json;
-    }
-
-    public JSONObject count() {
-        JSONObject json = new JSONObject();
-        json.put("datatype", 4);
-        json.put("type", "count");
-        json.put("data", String.valueOf(this.state.size()));
-        System.out.println("JSON to send: " + json);
-        return json;
-    }
-
-    public JSONObject switching(int num1, int num2){
-        JSONObject json = new JSONObject();
-        json.put("datatype", 5);
-        json.put("type", "switch");
-
-        try{
-            String index1 = state.strings.get(num1);
-            String index2 = state.strings.get(num2);
-
-            state.strings.set(num1,index2);
-            state.strings.set(num2,index1);
-
-            json.put("data","Success");
-        } catch(Exception e){
-            json.put("data","Error");
-        }
-
-        return json;
-    }
-
-    public static JSONObject error(String err) {
-        JSONObject json = new JSONObject();
-        json.put("error", err);
-        return json;
-    }
-
-    public void doPerform() {
-        boolean quit = false;
-        OutputStream out = null;
-        InputStream in = null;
+        port = -1;
         try {
-            out = conn.getOutputStream();
-            in = conn.getInputStream();
-            System.out.println("Server connected to client:");
-            while (!quit) {
-                byte[] messageBytes = NetworkUtils.receive(in);
-                JSONObject message = JsonUtils.fromByteArray(messageBytes);
-                JSONObject returnMessage = new JSONObject();
-   
-                int choice = message.getInt("selected");
-                    switch (choice) {
-                        case (0):
-                            returnMessage = quit();
-                            break;
-                        case (1):
-                            String inStr = (String) message.get("data");
-                            returnMessage = add(inStr);
-                            break;
-                        case (2):
-                            System.out.println("Popping...");
-                            returnMessage = pop();
-                            break;
-                        case (3):
-                            returnMessage = display();
-                            break;
-                        case (4):
-                            returnMessage = count();
-                            break;
-                        case (5):
-                            String data = (String) message.get("data");
-                            String[] params = data.split(" ");
-                            int num1 = Integer.parseInt(params[0]);
-                            int num2 = Integer.parseInt(params[1]);
-                            returnMessage = switching(num1-1, num2-1);
-                            break;
-                        default:
-                            returnMessage = error("Invalid selection: " + choice 
-                                    + " is not an option");
-                            break;
-                    }
-                // we are converting the JSON object we have to a byte[]
-                byte[] output = JsonUtils.toByteArray(returnMessage);
-                NetworkUtils.send(out, output);
+            port = Integer.parseInt(args[0]);
+        } catch (NumberFormatException nfe) {
+            System.out.println("[Port] must be an integer");
+            System.exit(2);
+        }
+        ServerSocket server = new ServerSocket(port);
+        System.out.println("Server Started...");
+        try {
+            while (true) {
+                System.out.println("Accepting a Request...");
+                Socket sock = server.accept();
+
+                Performer performer = new Performer(sock, strings);
+                performer.doPerform();
+                try {
+                    System.out.println("close socket of client ");
+                    sock.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-            // close the resource
-            System.out.println("close the resources of client ");
-            out.close();
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            System.out.println("Client disconnected:");
+        } finally {
+            try {
+                server.close();
+            } catch (Exception ex) {
+                System.out.println("Error while closing client connection...");
+            }
         }
     }
 }
